3c7ed68b13eb189a4d40ed1b060c16282b80c631
Mario Vera <callmejumpking@proton.me>
2024-02-29 20:03:34 -0600

    Did the exact same for the remaning two, backstage_tick and Sulfuras are now both classes

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -36,14 +36,12 @@ class GildedRose
       @days_remaining
   end
   def sulfuras_tick
+      @item = Sulfuras.new(quality,days_remaining)
+      item.tick
   end
   def backstage_tick
-      @days_remaining -=1
-      return if @quality >= 50
-      return @quality = 0 if @days_remaining < 0
-      @quality += 1
-      @quality += 1 if  @days_remaining < 10
-      @quality += 1 if  @days_remaining < 5
+      @item = Backstage.new(quality,days_remaining)
+      item.tick
   end
 end
 class Normal
@@ -70,4 +68,25 @@ class Brie
         @quality += 1
         @quality += 1 if @days_remaining <= 0
     end
+end
+class Sulfuras
+    attr_reader :quality, :days_remaining
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+    def tick
+    end
+end
+class Backstage
+    attr_reader :quality, :days_remaining
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+    def backstage_tick
+        @days_remaining -=1
+        return              if @quality >= 50
+        return @quality = 0 if @days_remaining < 0
+        @quality += 1
+        @quality += 1 if  @days_remaining < 10
+        @quality += 1 if  @days_remaining < 5
 end
